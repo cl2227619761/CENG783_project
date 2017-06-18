e7f604f915fdf8ce134cfe311a77648983c242d6
sysizlayan <sysizlayan@gmail.com>
2017-06-18 06:49:32 +0300

    arada commit unuttun, loss fonksiyonu hesaplanmadan hemen öncesi!

--- importDataSet.py
+++ importDataSet.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import matplotlib.image as mpimg
-from skimage import io
 
 
-itemNumbers=[0,0,0]
+itemNumbers = [0, 0, 0]
+
 
 def findTheFiles(location, verbose=0):
     sets = [location + "set1/",
@@ -55,9 +55,9 @@ def findTheFiles(location, verbose=0):
         diver_file.close()
     return total_number_of_images, nameDataset, itemNumbers
 
+
 def showSomeSamples(dict,verbose=0):
     numberofimages = len(dict.keys())
-    items = []
     fig, axes = plt.subplots(3, 3)
     fig.subplots_adjust(hspace=0.3, wspace=0.3)
     for i, ax in enumerate(axes.flat):
@@ -81,9 +81,10 @@ def showSomeSamples(dict,verbose=0):
         ax.set_yticks([])
     plt.show()
 
-def showObtainedImages(imageArray,labelArray,verbose=0):
-    numberofimages = len(imageArray)
 
+def showObtainedImages(imageArray,labelArray,verbose=0):
+
+    numberofimages = len(imageArray)
     fig, axes = plt.subplots(3, 3)
     fig.subplots_adjust(hspace=0.3, wspace=0.3)
 
@@ -107,10 +108,12 @@ def showObtainedImages(imageArray,labelArray,verbose=0):
         ax.set_yticks([])
     plt.show()
 
+
 def readTheImageAndResize(file_name):
     image_file = tf.read_file(file_name)
     image = tf.image.decode_png(image_file)
     return image
+
 
 def writeTfrecords(name_dataset, output_location, how_many_files=1000,totalFile=20,image_count=None,verbose=0):
     iteration=0
@@ -125,26 +128,28 @@ def writeTfrecords(name_dataset, output_location, how_many_files=1000,totalFile=
 
         if current_file % how_many_files == 0: # 100 by default
 
-            if writer: #close previously opened writer item
+            if writer:  # close previously opened writer item
                 writer.close()
             record_filename = "{a}maris{b}.tfrecords".format(a=output_location, b=iteration)
             print(record_filename)
 
-            fw = open(record_filename, "w") #to create the file
+            fw = open(record_filename, "w")  # only to create the file
             fw.close()
-            file_list.append(record_filename);
+
+            file_list.append(record_filename)
             writer = tf.python_io.TFRecordWriter(record_filename)
             print("Generated", record_filename)
 
             iteration += 1
 
-        if (image_count != None and current_file == image_count) or iteration == totalFile+1:
+        if (image_count is not None and current_file == image_count) or iteration == totalFile+1:
             break
-        #Read the image and decode
-        image = io.imread(file_name)
+        # Read the image and decode
+        image = mpimg.imread(file_name)
+
         # image = readTheImageAndResize(file_name)
         # image = sess.run(image)
-        image_resized = image[0:240, 0:320]  # crop unnecessary pixels
+        image_resized = image[0:240, 0:320]  # crop pixels to get good shape --> 240x320
         # fig = plt.figure()
         # plt.imshow(image_resized)
         # plt.show()
@@ -153,7 +158,7 @@ def writeTfrecords(name_dataset, output_location, how_many_files=1000,totalFile=
 
 
         feature = tf.train.Features(
-            feature = {'label': tf.train.Feature(bytes_list=tf.train.BytesList(value=[label_bytes])),
+            feature={'label': tf.train.Feature(bytes_list=tf.train.BytesList(value=[label_bytes])),
 
                        'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_bytes]))}
         )
@@ -169,10 +174,11 @@ def writeTfrecords(name_dataset, output_location, how_many_files=1000,totalFile=
     print("File conversion is done!")
     return file_list
 
+
 def readAndDecode(filename_queue):
     reader = tf.TFRecordReader()
     _, serialized_example = reader.read(filename_queue)
-    features = tf.parse_single_example(serialized_example,features={
+    features = tf.parse_single_example(serialized_example, features={
                                                             'label': tf.FixedLenFeature([], tf.string),
                                                             'image': tf.FixedLenFeature([], tf.string)
                                                             })
@@ -181,9 +187,10 @@ def readAndDecode(filename_queue):
 
     image_float = tf.reshape(image_flat,[240,320,3])
     image = tf.cast(image_float, dtype=tf.uint8)
-    label = tf.reshape(label_flat,[3,])
+    label = tf.reshape(label_flat, [3, ])
 
     return image, label
+
 
 def get_file_list(tf_record_location,):
     file_list = []
@@ -194,20 +201,20 @@ def get_file_list(tf_record_location,):
                 file_list.append(file_name_with_path)
     return file_list
 
-def getBatchFromFile(location, file_list = None, file_number=None, n_batch=1000,verbose=0):
-    sess = tf.InteractiveSession()
-    image_batch = np.zeros(shape=[n_batch, 240, 320, 3],dtype=np.uint8)
-    label_batch = np.zeros(shape=[n_batch, 3],dtype=np.float32)
-    if file_number != None:
+
+def getBatchFromFile(location, sess, file_list=None, file_number=None, n_batch=1000, verbose=0):
+    image_batch = np.zeros(shape=[n_batch, 240, 320, 3], dtype=np.uint8)
+    label_batch = np.zeros(shape=[n_batch, 3], dtype=np.float32)
+    if file_number is not None:
         filename_queue = tf.train.string_input_producer([location+"maris"+str(file_number)+".tfrecords"])
     else:
-        if file_list == None:
+        if file_list is None:
             file_list = get_file_list(location)
         filename_queue = tf.train.string_input_producer(file_list)
-        if verbose==0:
+        if verbose == 1:
             print(file_list)
     with tf.device('/cpu:0'):
-        image, label= readAndDecode(filename_queue)
+        image, label = readAndDecode(filename_queue)
         init_op = tf.global_variables_initializer()
         sess.run(init_op)
         coord = tf.train.Coordinator()
@@ -218,7 +225,10 @@ def getBatchFromFile(location, file_list = None, file_number=None, n_batch=1000,
             label_batch[i] = l
         coord.request_stop()
         coord.join(threads)
-    sess.close()
-    print("Batch is taken!")
-    return image_batch,label_batch
+    if verbose == 1:
+        print("Batch is taken!")
+    return image_batch, label_batch
 
+
+def importAllDataSet(location, number_of_images, session=None):
+    return getBatchFromFile(location, session, n_batch=number_of_images, verbose=0)
---
+++ weaklySupervisedObjectDetectionNetwork.py
@@ -0,0 +1,184 @@
+import tensorflow as tf
+import numpy as np
+
+class WSCNN:
+    def __init__(self, weight_file, sess=None, s=4):  # s is rescaling list variable
+        self.weightsAndBiases = np.load(weight_file)
+        self.keys = sorted(self.weightsAndBiases.keys())
+        print(self.keys)
+        self.rescaling_factors = np.array([0.5, 0.75, 1, 1.5])
+        self.original_image_size = np.array([320, 320], dtype=np.int32)
+        self.rescaled_sizes = np.zeros(shape=[s, 2])
+        for i in range(s):
+            self.rescaled_sizes[i] = np.array(self.original_image_size*self.rescaling_factors[i], dtype=np.uint32)
+
+        self.networkInitializer(sess, verbose=1)
+        print(self.rescaled_sizes)
+        self.weightsAndBiases = None  # trash out the unnecessary variable to save memory
+
+        self.batch = tf.placeholder(dtype=tf.uint8, shape=[None, 240, 320, 3], name="Input_batch")
+        self.labels = tf.placeholder(dtype=tf.float32, shape=[None, 3], name="True_labels")
+        self.calculateScores()
+
+    def networkInitializer(self, session,verbose=0):
+        with tf.device("/gpu:0"):
+            self.weights = {
+                'conv1_1': tf.Variable(self.weightsAndBiases['conv1_1_W']),
+                'conv1_2': tf.Variable(self.weightsAndBiases['conv1_2_W']),
+                'conv2_1': tf.Variable(self.weightsAndBiases['conv2_1_W']),
+                'conv2_2': tf.Variable(self.weightsAndBiases['conv2_2_W']),
+                'conv3_1': tf.Variable(self.weightsAndBiases['conv3_1_W']),
+                'conv3_2': tf.Variable(self.weightsAndBiases['conv3_2_W']),
+                'conv3_3': tf.Variable(self.weightsAndBiases['conv3_3_W'])
+
+                # 'conv4_1': tf.Variable(self.weightsAndBiases['conv4_1_W']),
+                # 'conv4_2': tf.Variable(self.weightsAndBiases['conv4_2_W']),
+                # 'conv4_3': tf.Variable(self.weightsAndBiases['conv4_3_W']),
+                # 'conv5_1': tf.Variable(self.weightsAndBiases['conv5_1_W']),
+                # 'conv5_2': tf.Variable(self.weightsAndBiases['conv5_2_W']),
+                # 'conv5_3': tf.Variable(self.weightsAndBiases['conv5_3_W'])
+            }
+            self.biases = {
+                'bias1_1': tf.Variable(self.weightsAndBiases['conv1_1_b']),
+                'bias1_2': tf.Variable(self.weightsAndBiases['conv1_2_b']),
+                'bias2_1': tf.Variable(self.weightsAndBiases['conv2_1_b']),
+                'bias2_2': tf.Variable(self.weightsAndBiases['conv2_2_b']),
+                'bias3_1': tf.Variable(self.weightsAndBiases['conv3_1_b']),
+                'bias3_2': tf.Variable(self.weightsAndBiases['conv3_2_b']),
+                'bias3_3': tf.Variable(self.weightsAndBiases['conv3_3_b'])
+
+                # 'bias4_1': tf.Variable(self.weightsAndBiases['conv4_1_b']),
+                # 'bias4_2': tf.Variable(self.weightsAndBiases['conv4_2_b']),
+                # 'bias4_3': tf.Variable(self.weightsAndBiases['conv4_3_b']),
+                # 'bias5_1': tf.Variable(self.weightsAndBiases['conv5_1_b']),
+                # 'bias5_2': tf.Variable(self.weightsAndBiases['conv5_2_b']),
+                # 'bias5_3': tf.Variable(self.weightsAndBiases['conv5_3_b'])
+            }
+            self.conv_out1 = tf.Variable(
+                                        tf.truncated_normal([10, 10, 256, 1024], dtype=tf.float32, stddev=1e-1),
+                                        trainable=True, name='Conv_like_fully1')
+
+            self.bias_out1 = tf.Variable(
+                                        tf.constant(0.0, shape=[1024], dtype=tf.float32),
+                                        trainable=True, name='Conv_like_fully1_bias')
+
+            self.conv_out2 = tf.Variable(
+                                        tf.truncated_normal([1, 1, 1024, 3], dtype=tf.float32, stddev=1e-1),
+                                        trainable=True, name='Conv_like_fully2')
+
+            self.bias_out2 = tf.Variable(
+                                        tf.constant(0.0, shape=[3], dtype=tf.float32),
+                                        trainable=True, name='Conv_like_fully2_bias')
+
+        init_op = tf.group(tf.global_variables_initializer(),
+                           tf.local_variables_initializer())
+        session.run(init_op)
+        if verbose == 1:
+            for key in self.weights.keys():
+                print("Name: {}, Shape = {}".format(key, self.weights[key].get_shape()))
+
+    def calculateScores(self):
+
+        # PREPROCESSING WILL COME HERE
+        # first convert images back to float32, convolution requires that
+        images = tf.image.convert_image_dtype(self.batch, dtype=tf.float32, name="uint8_2_float")
+        print(images.get_shape())
+
+        # zero padding from 240*320 to 320*320 for each image
+        image_padded = tf.pad(images, [[0, 0], [40, 40], [0, 0], [0, 0]], "CONSTANT", name="320X320_padding")
+        print(image_padded.get_shape())
+
+        rescaled_images_0 = tf.image.resize_images(image_padded, self.rescaled_sizes[0])
+        rescaled_images_1 = tf.image.resize_images(image_padded, self.rescaled_sizes[1])
+        rescaled_images_2 = tf.image.resize_images(image_padded, self.rescaled_sizes[2])
+        rescaled_images_3 = tf.image.resize_images(image_padded, self.rescaled_sizes[3])
+
+        netScores = tf.stack([
+                            self.network(rescaled_images_0),
+                            self.network(rescaled_images_1),
+                            self.network(rescaled_images_2),
+                            self.network(rescaled_images_3)], axis=1
+        )
+
+        self.overAllScore = tf.reduce_mean(netScores, axis=1)
+        print(self.overAllScore.get_shape())
+
+    def network(self, processed_batch, verbose=0):
+        ################################
+        # Convolution 1
+        ################################
+        conv = tf.nn.conv2d(processed_batch, self.weights['conv1_1'], [1, 1, 1, 1], padding='SAME')
+        out = tf.nn.bias_add(conv, self.biases['bias1_1'])
+        conv1_1 = tf.nn.relu(out, name='Convolutional1_1')
+
+        conv = tf.nn.conv2d(conv1_1, self.weights['conv1_2'], [1, 1, 1, 1], padding='SAME')
+        out = tf.nn.bias_add(conv, self.biases['bias1_2'])
+        conv1_2 = tf.nn.relu(out, name='Convolutional1_1')
+
+        pool1 = tf.nn.max_pool(conv1_2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME',
+                                    name='MaxPool2by2_1')
+        ################################
+        # Convolution 2
+        ################################
+        conv = tf.nn.conv2d(pool1, self.weights['conv2_1'], [1, 1, 1, 1], padding='SAME')
+        out = tf.nn.bias_add(conv, self.biases['bias2_1'])
+        conv2_1 = tf.nn.relu(out, name='Convolutional2_1')
+
+        conv = tf.nn.conv2d(conv2_1, self.weights['conv2_2'], [1, 1, 1, 1], padding='SAME')
+        out = tf.nn.bias_add(conv, self.biases['bias2_2'])
+        conv2_2 = tf.nn.relu(out, name='Convolutional2_2')
+
+        pool2 = tf.nn.max_pool(conv2_2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME',
+                                    name='MaxPool2by2_2')
+
+        ################################
+        # Convolution 3
+        ################################
+        conv = tf.nn.conv2d(pool2, self.weights['conv3_1'], [1, 1, 1, 1], padding='SAME')
+        out = tf.nn.bias_add(conv, self.biases['bias3_1'])
+        conv3_1 = tf.nn.relu(out, name='Convolutional3_1')
+
+        conv = tf.nn.conv2d(conv3_1, self.weights['conv3_2'], [1, 1, 1, 1], padding='SAME')
+        out = tf.nn.bias_add(conv, self.biases['bias3_2'])
+        conv3_2 = tf.nn.relu(out, name='Convolutional3_2')
+
+        conv = tf.nn.conv2d(conv3_2, self.weights['conv3_3'], [1, 1, 1, 1], padding='SAME')
+        out = tf.nn.bias_add(conv, self.biases['bias3_3'])
+        conv3_3 = tf.nn.relu(out, name='Convolutional3_3')
+
+        pool3 = tf.nn.max_pool(conv3_3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME',
+                                    name='MaxPool2by2_3')
+
+        conv = tf.nn.conv2d(pool3, self.conv_out1, [1, 1, 1, 1], padding="VALID")
+        out = tf.nn.bias_add(conv,self.bias_out1)
+        conv_out1 = tf.nn.relu(conv, name="FullyOut1")
+
+        conv = tf.nn.conv2d(conv_out1, self.conv_out2, [1, 1, 1, 1], padding="VALID")
+        out = tf.nn.bias_add(conv, self.bias_out2)
+        conv_out2 = tf.nn.relu(conv, name="FullyOut2")
+
+        global_max_pool = tf.reduce_max(conv_out2, [1, 2], name="GlobalMaxPool")
+
+        if verbose == 1:
+            print(conv1_1.get_shape())
+            print(conv1_2.get_shape())
+            print(pool1.get_shape())
+            print(conv2_1.get_shape())
+            print(conv2_2.get_shape())
+            print(pool2.get_shape())
+            print(conv3_1.get_shape())
+            print(conv3_2.get_shape())
+            print(conv3_3.get_shape())
+            print(conv_out1.get_shape())
+            print(conv_out2.get_shape())
+            print(global_max_pool.get_shape())
+
+        return global_max_pool
+
+# if __name__ == '__main__':
+#     sess = tf.Session()
+#     set = tf.placeholder(tf.float32, [None, None, None, 3])
+#     WSCNN = WSCNN(set, "./vgg16_weights.npz", sess)
+#     print(WSCNN.weights['conv1_1'].get_shape())
+#     print(sess.run(WSCNN.weights['conv1_1']))
+#     sess.close()
